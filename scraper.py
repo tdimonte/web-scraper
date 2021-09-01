77238cedce7086d817b7d7ba5b7b867ac1a6cf14
tdimonte <tdimonte@luc.edu>
2021-09-01 11:34:56 -0500

    Added Scraper.py request, products.csv

--- scraper.py
+++ scraper.py
@@ -1,9 +1,29 @@
-#Environment Setup (Python and pip paths)
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url_test = "https://us.theoodie.com/"
+url = "https://us.theoodie.com/"
 
-request = urlopen(url_test)
+request = urlopen(url)
 page_html = request.read()
 request.close()
+
+html_soup = BeautifulSoup(page_html, 'html.parser')
+
+items = html_soup.find_all('div', class_="grid-view-item")
+
+filename = 'products.csv'
+f = open(filename, 'w')
+
+headers = 'Title, Price, SalePrice \n'
+
+f.write(headers)
+
+#loop not working :/
+for val in items:
+    title = val.find('div', class_="h4 grid-view-item__title").text
+    price = val.find('span', class_="product-price__price").text
+    salePrice = val.find('span', class_="money").text
+
+f.write(title + ',' + price + ',' + salePrice)
+
+f.close()
